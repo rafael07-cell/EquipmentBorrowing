adcd8cdb107afe2d9f1b115e4cbb6b3f72eb6b8b
Raelin Guipetacio <rafaaelsofian@outlook.com>
2026-08-28 08:46:14 +0800

    Add domain models

---
+++ EquipmentBorrowing.Domain/Equipment.cs
@@ -0,0 +1,18 @@
+﻿namespace EquipmentBorrowing.Domain;
+
+public class Equipment
+{
+    public int Id { get; }
+    public string Name { get; }
+    public bool IsAvailable { get; private set; }
+
+    public Equipment(int id, string name, bool isAvailable = true)
+    {
+        Id = id;
+        Name = name;
+        IsAvailable = isAvailable;
+    }
+
+    public void MarkBorrowed() => IsAvailable = false;
+    public void MarkReturned() => IsAvailable = true;
+}
